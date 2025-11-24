34bb6f727ca37740c788ce9f0e0bb5813ea003a7
Nataniel Dziadzia <sumplous.sumplous@gmail.com>
2025-11-24 23:18:25 +0100

    first steps in learning rust Chapter 1

---
+++ getting_started/hello_world/main.rs
@@ -0,0 +1,11 @@
+/*
+ * compile with -> rustc [file_name]
+*/
+fn main() {
+    /*
+     * Calling rust macro (!)
+     * functions are without it
+     * the rest is standard C
+    */
+    println!("Hello, World!");
+}
