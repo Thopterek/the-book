e31aca92dd0f5b8d1ac4752ff5aa24faf9c5244c
Thopterek <sumplous.sumplous@gmail.com>
2025-12-04 12:47:48 +0100

    common data types, notes about tuples and arrays and how to access them

--- 3_common_programming_concepts/data_types/src/main.rs
+++ 3_common_programming_concepts/data_types/src/main.rs
@@ -65,5 +65,43 @@ fn main() {
     let x = 2.0;
     let y: f32 = 3.1;
     println!("Different floats addition {}", x + y);
+    /*
+     * Explicit type annotation can be done also for bools
+     * and the chars, the second one are c style ''
+     * if the values are unused you can prefix with _
+     * using the UCI standard rather than ASCII
+    */
+    let prawda = true;
+    let _falsz: bool = false;
+    let _character = 'c';
+    let explicit_char: char = 'c';
+    println!("Chars are like in {explicit_char} -> {prawda}");
+    /*
+     * Compund types, grouping multiple values in one type
+     * as for the basics we got tuple / array
+     * tuples have fixed lenght made on declaration
+     * you can explicitly type annotate through ([type], [another])
+     * the first part is called binding -> creating compund element
+     * to get individual we need to do destructure through another instance
+     * another way is to use period and the index of the accessed value
+     * tuple that is empty is an unit -> written as () -> empty value or empty return type
+    */
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (first, second, third) = tup;
+    println!("Can you print tuple? -> {first}, {second}, {third}\nYess by accessing individualy");
+    println!("Or with the small acces {}", tup.1);
+    /*
+     * Arrays in rust also have fixed lenght
+     * they have to have the same type like in C
+     * lives on the stack unlike Vector
+     * access through the [] in C style
+     * to make it explicit let [name]: [[type]; [size]] = [values, more_val]
+     * or with the same value through let [name]: [[value]; [how_many]]
+     * rust panics (exits) before accessing the memory over the bounds
+    */
+    let array = [1, 2, 3];
+    let array: [i32; 3] = array;
+    let array = [3; 3];
+    println!("Simple array with last value -> {}", array[2]);
 }
 
