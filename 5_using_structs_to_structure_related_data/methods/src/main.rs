4add250f6b6876a9c845577e742c778ae1ee6d83
Thopterek <sumplous.sumplous@gmail.com>
2025-12-15 00:57:22 +0100

    Trying out commit with LazyGit
    I think it might work even better than normal one for standard commit

---
+++ 5_using_structs_to_structure_related_data/methods/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("M E T H O D S");
+}
+
+/*
+ * Methods are part of the structs
+ * allowing to give special functionality
+ * first param is always self
+*/
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
