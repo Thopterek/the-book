33ddd383238540abd32c3ca8204c5ed9e920b1a4
Thopterek <sumplous.sumplous@gmail.com>
2025-12-14 22:15:33 +0100

    trying to make a  simple program with structs the lifetimes are coming later but the ownership being part of the structs sounds super intresting

--- 5_using_structs_to_structure_related_data/defining_and_instantiating_structs/src/main.rs
+++ 5_using_structs_to_structure_related_data/defining_and_instantiating_structs/src/main.rs
@@ -7,7 +7,7 @@ struct User {
     active: bool,
     username: String,
     email: String,
-    sign_in_count: u64,
+    _sign_in_count: u64,
 }
 
 fn create_user(active: bool, username: String, email: String, count: u64) -> User {
@@ -15,9 +15,22 @@ fn create_user(active: bool, username: String, email: String, count: u64) -> Use
         active: active,
         username: username,
         email: email,
-        sign_in_count: count,
+        _sign_in_count: count,
     };
     new_user
+}
+
+/*
+ * We can rewrite above function with Field init
+ * if it takes the exact same name it fills out on its own
+*/
+fn field_init_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        _sign_in_count: 1,
+    }
 }
 
 fn main() {
@@ -28,4 +41,38 @@ fn main() {
     println!("And now of the second user with email -> {}, active -> {}", user_two.email, user_two.active);
     user_two.active = true;
     println!("And then got changed to active -> {}, but email is still -> {}", user_two.active, user_two.email);
+    let filled_user = field_init_user(String::from("FILL UP"), String::from("Random Ass Email"));
+    println!("Damn it works -> {}", filled_user.username);
+    // Struct update syntax, allowing to fill out only a part to be changed
+    // but it still moves things around that don't have the copy trait like String
+    let remade_two = User {
+        email: String::from("WELL HE HAS EMAIL"),
+        username: String::from("Added Name"),
+        ..user_two
+    };
+    println!("Lets see remate_two -> {}, {}, {}", remade_two.email, remade_two.active, remade_two.username);
+    println!("Lets see user_two -> {}, {}, {}", user_two.email, user_two.active, user_two.username);
+    let pointxyz = Numbers(0, 0, 0);
+    // deconstruct but naming the type
+    let Numbers(x, y, z) = pointxyz;
+    println!("{x}, {y}, {z}");
+    let same = SameButDiff(0, 0, 0);
+    let SameButDiff(a, b, c) = same;
+    println!("{a}, {b}, {c}");
+    let _idk = WhatsGoingOn;
 }
+
+/*
+ * Those are Tuple Structs
+ * each has different type depending on struct name
+*/
+struct Numbers(i32, i32, i32);
+struct SameButDiff(i32, i32, i32);
+
+/*
+ * Unit Like Structs
+ * this thing type -> ()
+ * implementing a trait on some type but no storage
+*/
+struct WhatsGoingOn;
+
