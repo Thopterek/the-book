4cec3c8d01b9a2fe924756bfa8943ba763a09556
Thopterek <nataniel.dziadzia@gmail.com>
2025-12-17 12:15:54 +0100

    ending with if let part and the Option<T> type

--- 6_enums_and_patter_matching/concise_control_flow_with_if_let_and_let_else/src/main.rs
+++ 6_enums_and_patter_matching/concise_control_flow_with_if_let_and_let_else/src/main.rs
@@ -1,3 +1,10 @@
+use std::any::type_name;
+
+// extra type checking
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
 fn main() {
     println!("--------------");
     println!("Hello, if let!");
@@ -11,6 +18,7 @@ fn main() {
      * }
     */
     let config_max = Some(3u8);
+    println!("{}", type_of(&config_max));
     if let Some(max) = config_max {
         println!("The max is configured to be {max}");
     }
@@ -23,5 +31,22 @@ fn main() {
     match config_max {
         Some(max) => println!("The max is configured to be {max}"),
         _ => (),
+    }
+    // there is also let else as per
+    if let None = check(config_max) {
+        println!("There was nothing");
+    } else {
+        println!("There was a string returned");
+    }
+}
+
+fn check(passed: Option<u8>) -> Option<String> {
+    let Some(value) = passed else {
+        return None;
+    };
+    if value > 1 {
+        Some("Oh its bigger than one its".to_string())
+    } else {
+        Some("It was smaller than 1".to_string())
     }
 }
